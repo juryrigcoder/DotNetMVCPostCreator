5db8b84da5d18e2821602797d94f1bc31a74f959
Andrew <andrew@laytipster.co.uk>
2017-01-26 08:31:33 +0000

    Initial committee

---
+++ DotNetMVCPostCreator/demoMVC/Controllers/BlogPostController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.Mvc;
+using demoMVC.Domain;
+using demoMVC.Models;
+
+namespace demoMVC.Controllers
+{
+    public class BlogPostController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult BlogPostForm(string title, string blogpost)
+        {
+           ArticleModel
+            article = new ArticleModel()
+            {
+                ArticleId = 1,
+                ArticleBody = blogpost,
+                Title = title,
+                Created = DateTime.UtcNow
+            };
+            Consumables.ConsumablesInsert(article);
+            return RedirectToAction("Index");
+        }
+    }
+}
